0bbb478c9e7574f7d68b54d1387f0cde4ddefb60
fjohanssonnn <fjohansson.creative@gmail.com>
2024-04-12 17:07:13 +0200

    feat: defined props for each product and rendered product-information

--- src/components/product/ProductCard.vue
+++ src/components/product/ProductCard.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>
-
-  </div>
+  <article>
+    <img :src="product.image" :alt="product.title" />
+    <span>{{ product.category.toUpperCase() }}</span>
+    <h3>{{ product.title }}</h3>
+    <p>{{ product.description }}</p>
+  </article>
 </template>
 
-<script lang="ts">
-  
+<script setup lang="ts">
+  import type { Product } from "../../../types/types"
+
+  defineProps({
+    product: {
+      type: Object as () => Product,
+      required: true
+    }
+  })
 </script>
 
 <style scoped>

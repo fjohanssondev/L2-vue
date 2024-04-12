59ba02bbbc4f2378f7d2d8849c1ec4f1d326946a
fjohanssonnn <fjohansson.creative@gmail.com>
2024-04-12 16:51:19 +0200

    feat: added a component that fetches some products from the fake-api and set those to the products-state. Also render a ProductCard for each item and spread the props using v-bind

--- src/components/product/ProductList.vue
+++ src/components/product/ProductList.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-
-  </div>
+  <section>
+    <div v-for="product in products">
+      <ProductCard :key="product.id" v-bind="product" />
+    </div>
+  <p v-if="loading">Loading products...</p>
+  </section>
 </template>
 
 <script setup lang="ts">
+  import { ref } from "vue"
+  import type { Product } from "../../../types/types"
+  import ProductCard from "./ProductCard.vue"
 
+  const products = ref<Product[]>([])
+  const loading = ref(true)
+
+  const fetchProducts = async () => {
+    loading.value = true
+    
+    try {
+      const res = await fetch('https://fakestoreapi.com/products')
+      
+      if (res.ok){
+        const articles = await res.json()
+        products.value = articles
+        loading.value = false
+      }
+    } catch(err){
+      console.error(err)
+    }
+  }
+  
+  fetchProducts()
 </script>
 
 <style scoped>

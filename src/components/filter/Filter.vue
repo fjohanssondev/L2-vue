91e1218a085365b129a56bc05b71a11c7861e102
fjohanssonnn <fjohansson.creative@gmail.com>
2024-04-13 12:15:17 +0200

    feat: fetch categories to populate the select-box. The select-box takes the prop as it's value. When changed, it will emit the event 'update:filterValue' with the current value of the select-box

---
+++ src/components/filter/Filter.vue
@@ -0,0 +1,42 @@
+<template>
+  <select :value="$props.filterValue" @change="$emit('update:filterValue', ($event.target as HTMLSelectElement).value)">
+    <option disabled value="">Select category</option>
+    <option v-for="category in categories" :value="category">
+      {{ category.charAt(0).toUpperCase() + category.slice(1) }}
+    </option>
+  </select>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const categories = ref<string[]>([])
+
+const props = defineProps({
+  filterValue: {
+    type: String,
+    required: true
+  }
+})
+
+const emits = defineEmits(['update:filterValue'])
+
+const fetchCategories = async () => {
+  try {
+    const res = await fetch('https://fakestoreapi.com/products/categories')
+
+    if (res.ok){
+      const data = await res.json()
+      categories.value = data
+    }
+  } catch (err){
+    console.error(err)
+  }
+}
+
+fetchCategories()
+</script>
+
+<style scoped lang="scss">
+
+</style>

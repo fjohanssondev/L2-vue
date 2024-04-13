4c77b152ebf494961e8970fb1b77adab84624a1b
fjohanssonnn <fjohansson.creative@gmail.com>
2024-04-13 12:16:06 +0200

    feat: added the emit-event to the Filter-component and updates the state with the current value it emitted

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import ProductList from './components/product/ProductList.vue';
+import Filter from './components/filter/Filter.vue';
 
+const categoryRef = ref('')
 </script>
 
 <template>
-
+  <h1>Welcome to your store!</h1>
+  <header class="header">
+    <h2>Products</h2>
+    <Filter
+      :filter-value="categoryRef"
+      @update:filter-value="categoryRef = $event"
+    />
+  </header>
+  <ProductList :filter-value="categoryRef" />
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
